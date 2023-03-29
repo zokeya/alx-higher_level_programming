8b9504a778a13a6d1417b73ad21d186766418580
zokeya <zdokeya@gmail.com>
2023-03-29 12:29:10 +0300

    mandatory tasks

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 --creates the table unique_id with id defaulting to 1 and unique
 CREATE TABLE IF NOT EXISTS `unique_id` (
-	id` INT DEFAULT 1 UNIQUE,
-	`name` VARCHAR(256)
-	);
+    `id`   INT          DEFAULT 1 UNIQUE,
+    `name` VARCHAR(256)
+);
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
---creates the database hbtn_0d_usa and the table states
+--Creates the database hbtn_0d_usa and the table states
 --id INT unique, auto generated, can’t be null and is a primary key
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
-	PRIMARY KEY(`id`),
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(256) NOT NULL
-	);
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);
