470e6e3eaec720a4bb7a31328150b0d3a59093d1
zokeya <zdokeya@gmail.com>
2023-03-29 12:49:16 +0300

    mandatory tasks

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,2 @@
--- Creates a table called first_table in the current database
-USE `database_name`;
-CREATE TABLE IF NOT EXISTS `first_table` (
-	id INT,
-	name VARCHAR(256)
-	) ENGINE=INNODB;
+-- Creates a table called first_table with values id and name.
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
