9cfe65d7862fd42efa934dcea3d8880bb7d5e9d5
zokeya <zdokeya@gmail.com>
2023-03-29 08:24:14 +0300

    Mandatory Tasks

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+--lists all records of the table second_table ordered by score (top first)
+SELECT `score`, `name` 
+FROM `second_table`
+ORDER BY `score` DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+--lists all records of the table second_table with name value and score desc
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` IS NOT NULL
+ORDER BY `score` DESC;
