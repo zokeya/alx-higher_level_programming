b56026379a9e646e2fa4970a6b1cae8278c11242
zokeya <zdokeya@gmail.com>
2023-03-29 12:20:42 +0300

    mandatory tasks

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
---creates the database hbtn_0d_usa and the table cities
+-- Creates the database hbtn_0d_usa with the table cities.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`state_id` INT NOT NULL,
-	`name` VARCHAR(256) NOT NULL,
-	FOREIGN KEY (`state_id`)
-	REFERENCES `states`(`id`)
+    PRIMARY KEY(`id`),
+    `id`       INT          NOT NULL AUTO_INCREMENT,
+    `state_id` INT          NOT NULL,
+    `name`     VARCHAR(256) NOT NULL,
+    FOREIGN KEY(`state_id`)
+    REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
