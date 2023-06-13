f37942dd3b89a5fc6823ffa3cc13baba0acfec62
zokeya <zdokeya@gmail.com>
2023-06-14 01:08:40 +0300

    Mandatory tasks

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,9 +2,11 @@
 """Mylist class that inherists from list"""
 
 class Mylist(list):
-    """initialize the obj"""
+    """define a subclass of the list"""
     def __init__(self):
+        """initialize the object"""
         super().__init__()
 
     def print_sorted(self):
+        """prints a sorted list"""
         print(sorted(self))
