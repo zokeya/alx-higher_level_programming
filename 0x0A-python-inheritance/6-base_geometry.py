76bb47c21f77d5f5352dfbc966177e97120e5892
zokeya <zdokeya@gmail.com>
2023-06-14 10:12:35 +0300

    Mandatory tasks

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a Geometry class that raises an Exception"""
+
+class BaseGeometry:
+    """initialize the class"""
+    def __init__(self):
+        pass
+
+    """public instance method that raises an exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
