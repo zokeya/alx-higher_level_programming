c059e24cd07d44242d3c08514e6d2d2243e77b38
zokeya <zdokeya@gmail.com>
2023-06-14 09:20:50 +0300

    Mandatory tasks

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a funtion to check if object
+is an instance of inherited class"""
+def is_kind_of_class(obj, a_class):
+    """is True if the object is an instance of inherited class else False"""
+    return (isinstance((bj, a_class))
