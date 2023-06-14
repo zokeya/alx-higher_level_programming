7d85eb770301f1964e4ae7d750b72ac9c465566a
zokeya <zdokeya@gmail.com>
2023-06-14 10:37:53 +0300

    Mandatory tasks

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a Geometry class that raises an Exception"""
+
+class BaseGeometry:
+    """initialize the class"""
+    def __init__(self):
+        pass
+
+    """public instance method that raises an exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    """public instance methot that validates value as an integer greater than 0"""
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
